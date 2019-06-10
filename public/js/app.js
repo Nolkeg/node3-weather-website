4e7ef36ee1373306a3c0c1601cdfd2f00a892cae
Nolkeg <46750330+Nolkeg@users.noreply.github.com>
2019-06-10 15:26:56 +0700

    Set up app for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -5,10 +5,13 @@ const messageTwo = document.querySelector("#message-2");
 
 weatherform.addEventListener("submit", (e)=>{
     e.preventDefault();
+
     const location = search.value;
+
     messageOne.textContent = "Loading...";
     messageTwo.textContent = "";
-    fetch("http://localhost:3000/weather?adress="+ encodeURIComponent(location)).then((response)=>{
+
+    fetch("/weather?adress="+ encodeURIComponent(location)).then((response)=>{
     response.json().then((data)=>{
         if(data.error)
         {
